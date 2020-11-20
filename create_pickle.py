e829e4790ccd098cd24af305abb45450aeddfb5f
duxenmx <37263527+duxenmx@users.noreply.github.com>
2020-11-20 00:37:26 -0800

    Add files via upload

---
+++ create_pickle.py
@@ -0,0 +1,48 @@
+
+import pickle
+import os.path
+import json
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import logging
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.ERROR)
+
+creddir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'credentials')
+creddir_2 = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'credentials/dash_id.json')
+
+
+def get_calendar_service(creds_file, creddir):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+
+    if os.path.exists(os.path.join(creddir, 'token.pickle')):
+        with open(os.path.join(creddir, 'token.pickle'), 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                str(os.path.join(creddir, str(creds_file))), SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open(os.path.join(creddir, 'token.pickle'), 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds, cache_discovery=False)
+    return service
+
+
+with open(creddir_2, "r") as rdash_id:
+    data = json.load(rdash_id)
+
+
+get_calendar_service(data["G_Meetings"]["CREDENTIALS_FILE"], creddir)
